de032309fd12139a543966d620b894b44f7f218f
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-09-25 19:30:23 +0530

    Create Find_Frequency.java

---
+++ Find_Frequency.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static int BS(int[] arr,int key, int lo, int hi, boolean isfloor) {
+        int ans = -1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(arr[mid] == key) {
+                ans = mid;
+                if(isfloor) {
+                    lo = mid+1;
+                }
+                else {
+                    hi = mid-1;
+                }
+            }
+            else if(arr[mid] < key) {
+                lo = mid+1;
+            } 
+            else hi = mid-1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        int q = sc.nextInt();
+        int[] qr = new int[q];
+        for(int i = 0;i < q; i++) qr[i] = sc.nextInt();
+        
+        for(int ele: qr) {
+            int floorKey = BS(arr,ele, 0, n-1, true);
+            int ceilKey = BS(arr, ele, 0, n-1, false);
+            if(floorKey != -1 || ceilKey != -1)
+                System.out.println(floorKey - ceilKey+1);
+            else System.out.println(0);
+        }
+    }
+}
