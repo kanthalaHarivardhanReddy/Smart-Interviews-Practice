a9a3aa7507ddcb53ee2a652119cd5b90f146074b
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-09-25 19:47:31 +0530

    Update Find_Frequency.java

--- Find_Frequency.java
+++ Find_Frequency.java
@@ -44,3 +44,60 @@ public class Solution {
         }
     }
 }
+
+
+// Solution 5 - using the compressed and frequency list 
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        int q = sc.nextInt();
+        int[] qr = new int[q];
+        for(int i = 0;i < q; i++) qr[i] = sc.nextInt();
+        
+        List<Integer> comp = new ArrayList<>();
+        List<Integer> freq = new ArrayList<>();
+        
+        for(int i = 0;i < n; i++) {
+            int ind = freq.size()-1;
+            if(ind != -1 && arr[i] == comp.get(ind)) {
+                freq.set(ind, freq.get(ind)+1);
+            }
+            else {
+                comp.add(arr[i]);
+                freq.add(1);
+            }
+        }
+        
+        for(int ele: qr) {
+            int ind = BS(comp, ele);
+            if(ind != -1 ) System.out.println(freq.get(ind));
+            else System.out.println(0);
+        }
+    }
+    private static int BS(List<Integer> comp, int key) {
+        int ans = -1, lo = 0, hi = comp.size()-1;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(comp.get(mid) == key) {
+                return mid;
+            }
+            else if(comp.get(mid) < key) {
+                lo = mid+1;
+            }
+            else hi = mid-1;
+        }
+        return ans;
+    }
+}
