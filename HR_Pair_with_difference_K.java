938935b70fe4be68f468ba778228d0c06d783723
kanthalaHarivardhanReddy <63392391+kanthalaHarivardhanReddy@users.noreply.github.com>
2022-09-29 19:55:08 +0530

    Update HR_Pair_with_difference_K.java

--- HR_Pair_with_difference_K.java
+++ HR_Pair_with_difference_K.java
@@ -1,3 +1,4 @@
+// using hash set: N, N
 import java.io.*;
 import java.util.*;
 
@@ -7,6 +8,39 @@ public class Solution {
         for(int ele: arr) {
             if(set.contains(k+ele) || set.contains(ele-k)) return true;
             set.add(ele);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i < n;i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(diffwithk(arr, k));
+        }
+    }
+}
+
+
+// using two pointers: N, 1
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private static boolean diffwithk(int[] arr, int k) {
+        Arrays.sort(arr);
+        int i = 0, j = 1; 
+        while(j < arr.length && i < arr.length) {
+            if(i < j && Math.abs(arr[j]-arr[i]) == Math.abs(k)) {
+                return true;
+            }
+            else if(Math.abs(arr[j]-arr[i]) < Math.abs(k)) j++;
+            else i++;
         }
         return false;
     }
